425898c63e36055e01af8417f989b2f516f69bcd
Duber1234 <dfpesca@misena.edu.co>
2015-08-29 17:42:45 -0500

    aumento y disminucion de parrafo

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ and open the template in the editor.
         <script src="jquery-2.1.4.min.js"></script>
         <style>
             #primer_parrafo{
-                background-color: red;
+                    background-color: red;
+                 
             }
             
         </style>
@@ -20,10 +21,29 @@ and open the template in the editor.
         <p id="primer_parrafo">Primer parrafo</p><br>
         <p>Segundo parrafo</p><br>
         <p>Tercer parrafo</p>
+        <input value="+" type="button" id="aumentar" />
+         <input value="-" type="button" id="disminuir" />
         <script>
 //        alert($("#primer_parrafo").css("background-color"));
 
         $("#primer_parrafo").css("background-color","blue");
+        var aumenta = document.getElementById("aumentar").addEventListener("click",tamano,false);
+        var disminulle = document.getElementById("disminuir").addEventListener("click",tamano,false);
+        
+        function tamano(fun){
+          
+            if(fun.target==aumentar){
+              
+               var tamanoDef = parseInt($("#primer_parrafo").css("font-size")) ;
+           
+               $("#primer_parrafo").css("font-size",tamanoDef*2);
+            }else if(fun.target==disminuir){
+                var tamanoDef = parseInt($("#primer_parrafo").css("font-size")) ;
+           
+               $("#primer_parrafo").css("font-size",tamanoDef/2);
+            }
+        }
+        
         
         </script>
     </body>
